36dce0390c609b1c8b6e04742f887f0c9484c962
Emmanuel Obeng <emmallob14@gmail.com>
2024-02-03 00:51:45 +0000

    Make use of the morgan middleware to log the path of request and the time used in processing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // load the routes
 const routes = require('./helpers/routes');
+
+// middleware
+app.use(morgan('dev'));
 
 app.get('/', routes.baseRequest);
 
